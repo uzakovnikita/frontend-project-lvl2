f1c8382a116dc2a41fb5aa87ed23d9e189a1223b
uzakovnikita <nikitauzakov2@gmail.com>
2020-02-25 00:38:55 +0400

    init second step

--- src/commander.js
+++ src/commander.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import program from 'commander';
 program
-    .option('-V, --version  ', 'output the version number')
+    .version('-V, --version')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action(function(firstConfig, secondConfig) {
+        console.log(firstConfig);
+        console.log(secondConfig);
+    })
     .description('Compares two configuration files and shows a difference.')
 // eslint-disable-next-line no-undef
 export default program.parse(process.argv);
