5d34cbeae0a4f3e7502b0d47a85be40f41adf404
Nikita Uzakov <nikitauzakov2@gmail.com>
2020-02-26 16:27:59 +0400

    update third step with change index.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
-import diff from '..';
+import genDiff from '../';
 program
     .version('-V, --version')
     .option('-f, --format [type]', 'output format')
     .arguments('<firstConfig> <secondConfig>')
     .action(
         function (firstConfig, secondConfig) {
-            const result = diff(firstConfig, secondConfig);
-            result.map(iter => console.log(iter));
+            const result = genDiff(firstConfig, secondConfig);
+            return result;
         }
     )
     .description('Compares two configuration files and shows a difference.')
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import lodash from 'lodash';
+import { has, isEqual } from 'lodash';
 import { readFileSync }  from 'fs';
+const plus = (key) => `+ ${key}`;
+const minus = (key) => `- ${key}`;
 export default (firstConfig, secondConfig) => {
+    const result = {};
     const firstData = readFileSync(firstConfig, {encoding: 'utf-8'}, function(err, data) {
         if (err) {
             return err;
@@ -15,7 +18,22 @@ export default (firstConfig, secondConfig) => {
            return JSON.parse(data);
         }
     })
-    for (const iter of firstData)
-    return [firstData, secondData];
-
+    if (isEqual(firstData, secondData)) {
+        return firstData;
+    }
+    const keys = Object.keys(firstData);
+    for (const key of keys) {
+        if (has(secondData, key)) {
+            if (secondData[key] === firstData[key]) {
+                result[key] = firstData[key];
+            } else {
+                result[plus(key)] = secondData[key];
+                result[minus(key)] = firstData[key];
+            }
+        } else {
+            result[minus(key)] = firstData[key];
+        }
+    }
+    return result;
 }
+
