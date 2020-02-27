c4dc81ad7c8e8d95a9f42435a8421fedbff0ffd9
Nikita Uzakov <nikitauzakov2@gmail.com>
2020-02-27 16:26:28 +0400

    update default function in index.js, where was changed style of cod from imperative for functional

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { has, isEqual } from 'lodash';
 import { readFileSync }  from 'fs';
 const plus = (key) => `+ ${key}`;
 const minus = (key) => `- ${key}`;
+
 export default (firstConfig, secondConfig) => {
     const result = {};
     const firstData = JSON.parse(readFileSync(firstConfig))
@@ -10,39 +11,17 @@ export default (firstConfig, secondConfig) => {
         return firstData;
     }
     const keys = Object.keys(firstData);
-    console.log(typeof(firstData))
-    // const validKeys = keys.filter((key)=> has(secondData, key));
-    // const invalidKeys = keys.filter((key) => !has(secondData, key));
-    // validKeys.map((key) => {
-    //     if (secondData[key] === firstData[key]) {
-    //         result[key] = firstData[key];
-    //     } else {
-    //         result[plus(key)] = secondData[key];
-    //         result[minus(key)] = firstData[key];
-    //     }
-    // })
-    // invalidKeys.map((key) => result[minus(key)] = firstData[key]);
-    for (const key of keys) {
-        if (has(secondData, key)) {
-            console.log(key)
-            console.log('secondData[key]')
-            console.log(secondData[key]);
-            console.log('firstData[key]');
-            console.log(firstData[key]);
-            console.log(secondData[key] === firstData[key])
-            if (secondData[key] == firstData[key]) {
-                console.log('hello')
-                console.log(key)
-                result[key] = firstData[key];
-                console.log(result[key])
-            } else {
-                result[plus(key)] = secondData[key];
-                result[minus(key)] = firstData[key];
-            }
+    const validKeys = keys.filter((key)=> has(secondData, key));
+    const invalidKeys = keys.filter((key) => !has(secondData, key));
+    validKeys.map((key) => {
+        if (isEqual(secondData[key], firstData[key])) {
+            result[key] = firstData[key];
         } else {
+            result[plus(key)] = secondData[key];
             result[minus(key)] = firstData[key];
         }
-    }
+    })
+    invalidKeys.map((key) => result[minus(key)] = firstData[key]);
     return result;
 }
 
