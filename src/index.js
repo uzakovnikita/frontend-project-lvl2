e2b1a05d1b6fe8c301c5e52b0675e07c2a5a16f5
Nikita Uzakov <nikitauzakov2@gmail.com>
2020-02-28 17:07:09 +0400

    update index.js: new added normalization of file path usage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import { has, isEqual } from 'lodash';
 import { readFileSync }  from 'fs';
+import { cwd } from 'process';
+import { resolve } from 'path';
 const plus = (key) => `+ ${key}`;
 const minus = (key) => `- ${key}`;
 
 export default (firstConfig, secondConfig) => {
+    const currentDirectory = cwd();
+    const firstSource = resolve(currentDirectory, firstConfig);
+    const secondSource = resolve(currentDirectory, secondConfig);
     const result = {};
-    const firstData = JSON.parse(readFileSync(firstConfig))
-    const secondData = JSON.parse(readFileSync(secondConfig))
+    const firstData = JSON.parse(readFileSync(firstSource))
+    const secondData = JSON.parse(readFileSync(secondSource))
     if (isEqual(firstData, secondData)) {
         return firstData;
     }
     const keys = Object.keys(firstData);
-    const validKeys = keys.filter((key)=> has(secondData, key));
-    const invalidKeys = keys.filter((key) => !has(secondData, key));
-    validKeys.map((key) => {
+    const matchingKeys = keys.filter((key)=> has(secondData, key));
+    const missmathcingKeys = keys.filter((key) => !has(secondData, key));
+    matchingKeys.map((key) => {
         if (isEqual(secondData[key], firstData[key])) {
             result[key] = firstData[key];
         } else {
@@ -21,7 +26,7 @@ export default (firstConfig, secondConfig) => {
             result[minus(key)] = firstData[key];
         }
     })
-    invalidKeys.map((key) => result[minus(key)] = firstData[key]);
+    missmathcingKeys.map((key) => result[minus(key)] = firstData[key]);
     return result;
 }
 
