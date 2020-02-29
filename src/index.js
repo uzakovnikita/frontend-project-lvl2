1bd177ab16b14b3740ca87a7fbeb5e3cd7a1b1c2
uzakovnikita <nikitauzakov2@gmail.com>
2020-02-29 17:58:51 +0400

    fix bags in third step

--- src/index.js
+++ src/index.js
@@ -3,31 +3,34 @@ import { readFileSync }  from 'fs';
 import { cwd } from 'process';
 import { resolve } from 'path';
 
-const plus = (key) => `  + ${key}`;
-const minus = (key) => `  - ${key}`;
-const space = (key) => `    ${key}`;
+const plus = (key) => `+ ${key}`;
+const minus = (key) => `- ${key}`;
+const space = (key) => `  ${key}`;
 const isObject = (obj, key) =>  Object.prototype.toString.call(obj[key]) === '[object Object]';
 
 const convertObjectToSring = (obj) => {
     const keys = Object.keys(obj);
     const list = keys.reduce((acc, key) => {
         if (isObject(obj, key)) {
-            acc = [...acc, convertObjectToSring(obj[key])];
+            acc = [...acc, `${key}: ${(convertObjectToSring(obj[key]))}`];
             return acc;
         }
-        return acc = [...acc, `${key}: ${obj[key]}`];
+        return acc = [...acc, `${space(key)}: ${(obj[key])}`];
     }, []);
-    const result = `{\n${list.join('\n')} \n}`;
+    const result = `\n${list.join('\n')} \n`;
     return result;
 }
+
 const diff = (firstData, secondData) => {
     if (isEqual(firstData, secondData)) {
       return firstData;
     }
     let difference = {};
     const keys = Object.keys(firstData);
+    const keys2 = Object.keys(secondData);
     const matchingKeys = keys.filter((key)=> has(secondData, key));
-    const missmathcingKeys = keys.filter((key) => !has(secondData, key));
+    const addedKeys = keys2.filter((key2) => !has(firstData, key2));
+    const deletedKeys = keys.filter((key) => !has(secondData, key));
     matchingKeys.map((key) => {
         if (isEqual(secondData[key], firstData[key])) {
             difference[space(key)] = firstData[key];
@@ -38,9 +41,10 @@ const diff = (firstData, secondData) => {
             difference[minus(key)] = firstData[key];
         }
     })
-    missmathcingKeys.map((key) => difference[minus(key)] = firstData[key]);
+    addedKeys.map((key)=> difference[plus(key)] = secondData[key]);
+    deletedKeys.map((key) => difference[minus(key)] = firstData[key]);
     const result = difference;
-    return convertObjectToSring(result);
+    return result;
 }
 
 export default (firstConfig, secondConfig) => {
@@ -49,7 +53,7 @@ export default (firstConfig, secondConfig) => {
     const secondSource = resolve(currentDirectory, secondConfig);
     const firstData = JSON.parse(readFileSync(firstSource))
     const secondData = JSON.parse(readFileSync(secondSource))
-    const result = diff(firstData, secondData);
+    const result = `{\n${convertObjectToSring(diff(firstData, secondData))}\n}`;
     return result;
 }
 
