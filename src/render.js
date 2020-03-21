95f9a690c9d6e0f9b8195de5516b8ee6f4356b66
uzakovnikita <nikitauzakov2@gmail.com>
2020-03-21 21:04:28 +0400

    update four step

--- src/render.js
+++ src/render.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-else-return */
 import { isObject } from 'lodash';
 
 const render = (ast, deep = 1) => {
@@ -6,28 +5,26 @@ const render = (ast, deep = 1) => {
   const str = ast.reduce((acc, current) => {
     if (!Array.isArray(current.value)) {
       if (current.type === 'notDiff') {
-        const newAcc = `${space.repeat(deep)}  ${current.key}: ${current.value}\n`;
+        const newAcc = `${space.repeat(deep)}${space}${current.key}: ${current.value}\n`;
         return acc.concat(newAcc);
       }
       if (isObject(current.value)) {
-        const x = JSON.stringify(current.value).replace(/[.{}"]/g, '');
-        const x2 = x.replace(/:/g, ': ');
-        const newString = `{\n${space.repeat(deep + 3)}${x2}\n  ${space.repeat(deep)}}`;
+        const stringify = JSON.stringify(current.value).replace(/[.{}"]/g, '');
+        const doneStr = stringify.replace(/:/g, ': ');
+        const newDeep = deep + 2;
+        const newString = `{\n${space.repeat(newDeep)}${space}${doneStr}\n${space}${space.repeat(deep)}}`;
         const newAcc = `${space.repeat(deep)}${current.type} ${current.key}: ${newString}\n`;
         return acc.concat(newAcc);
       }
       const newAcc = `${space.repeat(deep)}${current.type} ${current.key}: ${current.value}\n`;
       return `${acc.concat(newAcc)}`;
-    } else {
-      const newDeep = deep + 1;
-      const newAcc = `${space.repeat(newDeep)}${current.key}: {\n${render(current.value, newDeep + 1)}${space.repeat(newDeep)}}\n`;
-      return acc.concat(newAcc);
     }
+    const newDeep = deep + 2;
+    const newAcc = `${space.repeat(deep)}${space}${current.key}: {\n${render(current.value, newDeep)}${space.repeat(deep)}${space}}\n`;
+    return acc.concat(newAcc);
   }, '');
   return str;
 };
-const x = (str) => {
-  return `{\n${render(str)}}`;
-};
+const x = (str) => `{\n${render(str)}}`;
 export default x;
 // export default (ast) => ast;
