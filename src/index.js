51ad2b396efb7ec3e0574ecaaf1487400fc5df5a
uzakovnikita <55103524+uzakovnikita@users.noreply.github.com>
2020-04-16 07:44:54 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import parse from './parser';
 import diff from './diff';
 import format from './formatters';
 
-export default (firstSource, secondSource, format) => {
-  const firstData = parse(firstSource);
-  const secondData = parse(secondSource);
+export default (firstPath, secondPath, format) => {
+  const firstData = parse(firstPath);
+  const secondData = parse(secondPath);
   const difference = diff(firstData, secondData);
   const result = format(format, difference);
   return result;
