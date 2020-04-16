5aca2b11a393be651d0425f7501e2a134eefad79
uzakovnikita <55103524+uzakovnikita@users.noreply.github.com>
2020-04-16 07:43:43 +0400

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,30 +4,30 @@ import { readFileSync } from 'fs';
 import { safeLoad } from 'js-yaml';
 import { parse } from 'ini';
 
-const toParseFromJson = (source) => {
+const parseFromJson = (source) => {
   const firstData = JSON.parse(readFileSync(source));
   return firstData;
 };
 
-const toParseFromYml = (firstSource) => {
+const parseFromYml = (firstSource) => {
   const firstData = safeLoad(readFileSync(firstSource));
   return firstData;
 };
 
-const toParseFromIni = (source) => parse(readFileSync(source, 'utf-8'));
+const parseFromIni = (source) => parse(readFileSync(source, 'utf-8'));
 
 const parserManager = {
-  json: toParseFromJson,
-  yml: toParseFromYml,
-  ini: toParseFromIni,
+  json: parseFromJson,
+  yml: parseFromYml,
+  ini: parseFromIni,
 };
 
-export default (config) => {
+export default (path) => {
   const currentDirectory = cwd();
-  const source = resolve(currentDirectory, config);
+  const absolutePath = resolve(currentDirectory, path);
   const type = extname(source).replace(/[.]/g, '').trim();
   if (!({}).hasOwnProperty.call(parserManager, type)) {
     throw new Error(`Uknown type ${type}`);
   }
-  return parserManager[type](source);
+  return parserManager[type](absolutePath);
 };
