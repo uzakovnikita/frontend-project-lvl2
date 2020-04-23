391392f1a2a60c8a081706800deb13f90d373e3e
uzakovnikita <nikitauzakov2@gmail.com>
2020-04-23 22:49:11 +0400

    update render

--- src/formatters/render.js
+++ src/formatters/render.js
@@ -1,20 +1,20 @@
 import { isObject } from 'lodash';
 
 const renderValue = (value, deep) => {
-  const space = '  ';
-  const symbol = space;
   if (!isObject(value)) {
     return value;
   }
+  const space = '  ';
+  const symbol = space;
   const newDeep = deep + 2;
   const entries = Object.entries(value);
-  const newStr = entries.reduce((accum, element) => {
-    const [key, valued] = element;
-    const newAcc = `${space.repeat(newDeep)}${symbol}${key}: ${valued}\n`;
-    return accum.concat(newAcc);
-  }, '');
-  const newString = `{\n${newStr}${space}${space.repeat(deep)}}`;
-  return newString;
+  const newStr = entries.map((current) => {
+    const [key, val] = current;
+    const result = `${space.repeat(newDeep)}${symbol}${key}: ${val}`;
+    return result;
+  }).join('\n');
+  const result = `{\n${newStr}\n${space}${space.repeat(deep)}}`;
+  return result;
 };
 
 const render = (tree) => {
@@ -24,7 +24,7 @@ const render = (tree) => {
       switch (current.type) {
         case 'notDiff': {
           const symbol = space;
-          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${current.value}\n`;
+          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${current.value}`;
           return newString;
         }
         case 'changed': {
@@ -33,27 +33,27 @@ const render = (tree) => {
           const firstSymbol = '+ ';
           const secondSymbol = '- ';
           const firstString = `${space.repeat(deep)}${firstSymbol}${current.key}: ${newValue}\n`;
-          const secondString = `${space.repeat(deep)}${secondSymbol}${current.key}: ${oldValue}\n`;
+          const secondString = `${space.repeat(deep)}${secondSymbol}${current.key}: ${oldValue}`;
           const newString = firstString.concat(secondString);
           return newString;
         }
         case 'added': {
           const symbol = '+ ';
           const newValue = renderValue(current.value, deep);
-          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${newValue}\n`;
+          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${newValue}`;
           return newString;
         }
         case 'deleted': {
           const symbol = '- ';
           const newValue = renderValue(current.value, deep);
-          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${newValue}\n`;
+          const newString = `${space.repeat(deep)}${symbol}${current.key}: ${newValue}`;
           return newString;
         }
         case 'parent': {
           const symbol = space;
           const newDeep = deep + 2;
-          const children = iter(current.children, newDeep).join('');
-          const newString = `${space.repeat(deep)}${symbol}${current.key}: {\n${children}${space.repeat(deep)}${space}}\n`;
+          const children = iter(current.children, newDeep).join('\n');
+          const newString = `${space.repeat(deep)}${symbol}${current.key}: {\n${children}\n${space.repeat(deep)}${space}}`;
           return newString;
         }
         default:
@@ -62,8 +62,8 @@ const render = (tree) => {
     });
     return result;
   };
-  const result = iter(tree, 1).join('');
-  return `{\n${result}}`;
+  const result = iter(tree, 1).join('\n');
+  return `{\n${result}\n}`;
 };
 
 export default render;
